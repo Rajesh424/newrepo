afad1a36f386901406fb31f7411bbe98d1a63509
Rajesh424 <manyam.rajesh1@gmail.com>
2020-01-26 23:54:20 +0530

    this is firsh file

---
+++ First.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class First{
+	public static void main(String[] args)throws IOException {
+		FileWriter fw=new FileWriter("D:/java/out.txt");
+		PrintWriter pw=new PrintWriter(fw);
+		pw.write(100);
+		pw.print("hellow");
+		pw.flush();
+	}
+	
+	
+}
