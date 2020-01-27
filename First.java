d134f81adedd3af84540b111dbe6864cefff24e5
Rajesh424 <manyam.rajesh1@gmail.com>
2020-01-27 19:38:43 +0530

    clean

--- First.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-class First{
-	public static void main(String[] args)throws IOException {
-		FileWriter fw=new FileWriter("D:/java/out.txt");
-		PrintWriter pw=new PrintWriter(fw);
-		pw.write(100);
-		pw.print("hellow");
-		pw.flush();
-	}}
